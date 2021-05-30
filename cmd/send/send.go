d4aa5794c03cb15c69dbfdde000dc3917015d744
gyllone <gyl19930522@163.com>
2021-05-30 22:00:34 +0800

    update

--- cmd/send/send.go
+++ cmd/send/send.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"eth-trasnsfer/lib"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/urfave/cli/v2"
+	"math/rand"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -145,6 +149,178 @@ var sendERC20Cmd = &cli.Command{
 		} else {
 			return sender.SendERC20(cctx.Context, contract, si)
 		}
+	},
+}
+
+var batchSendETHCmd = &cli.Command{
+	Name:  "batch-send-eth",
+	Usage: "batch send eth",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "to-path",
+			Usage: "addresses file path",
+		},
+		&cli.Float64Flag{
+			Name:  "min-amount",
+			Usage: "min amount",
+		},
+		&cli.Float64Flag{
+			Name:  "max-amount",
+			Usage: "max amount",
+		},
+		&cli.Int64Flag{
+			Name: "interval",
+			Usage: "sending interval (seconds)",
+			Value: 1,
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		if !cctx.IsSet("pk") {
+			return errors.New("--pk is not passed")
+		}
+		if !cctx.IsSet("url") {
+			return errors.New("--url is not passed")
+		}
+		if !cctx.IsSet("to-path") {
+			return errors.New("--to-path is not passed")
+		}
+		if !cctx.IsSet("min-amount") {
+			return errors.New("--min-amount is not passed")
+		}
+		if !cctx.IsSet("max-amount") {
+			return errors.New("--max-amount is not passed")
+		}
+
+		pk, err := lib.HexToPk(cctx.String("pk"))
+		if err != nil {
+			return err
+		}
+
+		sender, err := lib.NewSender(cctx.Context, cctx.String("url"), pk, cctx.Float64("multiplier"))
+		if err != nil {
+			return err
+		}
+
+		toFile, err := os.Open(cctx.String("to-path"))
+		if err != nil {
+			return fmt.Errorf("open to file error: %v", err)
+		}
+		defer toFile.Close()
+
+		a := cctx.Float64("min-amount")
+		b := cctx.Float64("max-amount") - a
+		interval := time.Duration(cctx.Int64("interval")) * time.Second
+
+		scanner := bufio.NewScanner(toFile)
+		for scanner.Scan() {
+			<-time.After(interval)
+			to := common.HexToAddress(scanner.Text())
+			amount := strconv.FormatFloat(a + rand.Float64() * b, 'f', 10, 64)
+			si, err := lib.NewSendInfo(to, amount, 18)
+			if err != nil {
+				return err
+			}
+			err = sender.SendETH(cctx.Context, si)
+			if err != nil {
+				return err
+			} else {
+				fmt.Printf("send eth %s to %s ok\n", amount, to)
+			}
+		}
+		return nil
+	},
+}
+
+var batchSendERC20Cmd = &cli.Command{
+	Name:  "batch-send-erc20",
+	Usage: "batch send erc20",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "contract",
+			Usage: "erc20 contract address",
+		},
+		&cli.StringFlag{
+			Name:  "to-path",
+			Usage: "addresses file path",
+		},
+		&cli.Float64Flag{
+			Name:  "min-amount",
+			Usage: "min amount",
+		},
+		&cli.Float64Flag{
+			Name:  "max-amount",
+			Usage: "max amount",
+		},
+		&cli.Int64Flag{
+			Name: "interval",
+			Usage: "sending interval (seconds)",
+			Value: 1,
+		},
+		&cli.Int64Flag{
+			Name:  "prec",
+			Usage: "token precision",
+			Value: 18,
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		if !cctx.IsSet("contract") {
+			return errors.New("--contract is not passed")
+		}
+		if !cctx.IsSet("pk") {
+			return errors.New("--pk is not passed")
+		}
+		if !cctx.IsSet("url") {
+			return errors.New("--url is not passed")
+		}
+		if !cctx.IsSet("to-path") {
+			return errors.New("--to-path is not passed")
+		}
+		if !cctx.IsSet("min-amount") {
+			return errors.New("--min-amount is not passed")
+		}
+		if !cctx.IsSet("max-amount") {
+			return errors.New("--max-amount is not passed")
+		}
+
+		pk, err := lib.HexToPk(cctx.String("pk"))
+		if err != nil {
+			return err
+		}
+
+		sender, err := lib.NewSender(cctx.Context, cctx.String("url"), pk, cctx.Float64("multiplier"))
+		if err != nil {
+			return err
+		}
+
+		toFile, err := os.Open(cctx.String("to-path"))
+		if err != nil {
+			return fmt.Errorf("open to file error: %v", err)
+		}
+		defer toFile.Close()
+
+		contract := common.HexToAddress(cctx.String("contract"))
+		a := cctx.Float64("min-amount")
+		b := cctx.Float64("max-amount") - a
+		interval := time.Duration(cctx.Int64("interval")) * time.Second
+		prec := cctx.Int64("prec")
+
+		scanner := bufio.NewScanner(toFile)
+		for scanner.Scan() {
+			<-time.After(interval)
+			to := common.HexToAddress(scanner.Text())
+			amount := strconv.FormatFloat(a + rand.Float64() * b, 'f', 10, 64)
+			si, err := lib.NewSendInfo(to, amount, prec)
+			if err != nil {
+				return err
+			}
+			err = sender.SendERC20(cctx.Context, contract, si)
+			if err != nil {
+				return err
+			} else {
+				fmt.Printf("send erc20 token %s to %s ok\n", amount, to)
+			}
+		}
+		return nil
 	},
 }
 
